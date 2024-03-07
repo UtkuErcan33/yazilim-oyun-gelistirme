9571674971cb5bde3842b12e38789e5fab9e2122
Utku Mustafa ERCAN <utku.ercan.3368@gmail.com>
2024-03-07 14:25:43 +0300

    biyografi oluşturma
    biyografi oluşturmayı tamamladım bu kodları kullanarak kişi sayısını arttırarak istediğiniz sayıda kişinin biyografisini barındırabilceniz bir alan oluşturdum

--- Oyun Geliştirme Projelerim/unity oyun/inspimoders1/Assets/biyografi/kod/kodlama.cs
+++ Oyun Geliştirme Projelerim/unity oyun/inspimoders1/Assets/biyografi/kod/kodlama.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +8,17 @@ using UnityEngine.UI;
 public class kodlama : MonoBehaviour
 {
     public Text degisenmetin;
-    public Text isimmetni;
     public Text telefonmetni;
     public Text emailmetni;
     public Text konummetni;
-    public Text hakkýmdametni;
+    public Text linkedinmetni;
+    public Dropdown seçim;
+    public Image resimler;
+    public Sprite utku;
+    public Sprite birlikte;
+    public Sprite esma;
+
+
 
     void Start()
     {
@@ -19,34 +27,66 @@ public class kodlama : MonoBehaviour
     }
     void Update()
     {
-
-    }
-    public void telefon(string telefon)
-    {
-        telefonmetni.text = telefon;
-        degisenmetin.text = "05345442440";
-    }
-    public void email(string email)
-    {
-        degisenmetin.text = "utku.ercan.3368@gmail.com";
-    }
-    public void konum(string konum)
-    {
-        degisenmetin.text = "Mersin/Erdemli";
-    }
-    public void hakkýmda(string hakkýmda)
-    {
-        degisenmetin.text = "Fýrat ünisinde Yazýlým mühendisliðinde okuyorum 1. sýnýfým ilk senemde kazandým ve ilk senemde hazýrlýk okudum oyun geliþtiricisi olmak istiyorum";
+        fotogetirme();
     }
 
+   
 
     public void acýlýs()
     {
-        isimmetni.text = "UTKU MUSTAFA ERCAN";
+      
         degisenmetin.text = "Merhabalar ";
         telefonmetni.text = "TELEFON NUMARAM";
         emailmetni.text = "E-mail";
         konummetni.text = "Yaþadýðým Yer";
-        hakkýmdametni.text = "Hakkýmda";
+        linkedinmetni.text = "linkedin adresi";
+        
+    }
+
+      
+
+
+
+
+
+    public void utkunun()
+    {
+        telefonmetni.text = "05345442440";
+        emailmetni.text = "utku.ercan.3368@gmail.com";
+        konummetni.text = "Mersin/Erdemli";
+        linkedinmetni.text = "utku.ercan.3368";
+        degisenmetin.text = "Fýrat ünisinde Yazýlým mühendisliðinde okuyorum 1. sýnýfým ilk senemde kazandým ve ilk senemde hazýrlýk okudum oyun geliþtiricisi olmak istiyorum";
+        
+            }
+
+
+    public void esmanýn()
+    {
+        telefonmetni.text = "05367970904";
+        emailmetni.text = "esmaberfin4114@gmail.com";
+        konummetni.text = "Kocaeli/Ýzmit";
+        linkedinmetni.text = "esma.berfin.4114";
+        degisenmetin.text = "Fýrat ünisinde Yazýlým mühendisliðinde okuyorum 1. sýnýfým 1 sene mezuna kaldým ve yapay zeka alanýnda ilerlemek istiyorum yaný sýra grafik tasraýmýda yapmayý istiyorum"; 
+
+
+    }
+    public void fotogetirme()
+    {
+        if (seçim.value==0)
+        {
+            resimler.sprite = birlikte;
+            acýlýs();
+        }
+        else if (seçim.value == 1)
+        {
+            resimler.sprite = utku;
+
+            utkunun();
+        }
+        else
+        {
+            resimler.sprite = esma;
+            esmanýn();
+        }
     }
 }
